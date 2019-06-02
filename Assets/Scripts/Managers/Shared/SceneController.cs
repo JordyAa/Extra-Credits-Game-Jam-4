6639961b4081d055e06f17a987d7cd2261dea6f1
Jordy Aaldering <aalderingjordy@gmail.com>
2019-06-02 21:34:57 +0200

    scene transitions

--- Assets/Scripts/Managers/Shared/SceneController.cs
+++ Assets/Scripts/Managers/Shared/SceneController.cs
@@ -1,11 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public static event Action OnSceneEnd = delegate { };
+    
     public void LoadScene(int buildIndex)
     {
+        StartCoroutine(Load(buildIndex));
+    }
+
+    private static IEnumerator Load(int buildIndex)
+    {
         Time.timeScale = 1f;
+        
+        OnSceneEnd();
+        OnSceneEnd = delegate { };
+
+        yield return new WaitForSeconds(1f);
+        
         SceneManager.LoadScene(buildIndex);
     }
 
---
+++ Assets/Scripts/State Machines/PlayOnSceneEnd.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayOnSceneEnd : StateMachineBehaviour
+{
+    private Animator animator;
+    private static readonly int Scene = Animator.StringToHash("endScene");
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        this.animator = animator;
+        SceneController.OnSceneEnd += EndScene;
+    }
+
+    private void EndScene()
+    {
+        animator.SetTrigger(Scene);
+    }
+}
